4f2528ba29796514e9f202e6324b1f0c54cda7bb
ethankeystone <ethankeystone@live.com>
2020-01-16 13:37:16 -0500

    new website

--- README.md
+++
@@ -1,2 +0,0 @@
-# ethankeystone.github.io
-Personal Website
---
+++ precache-manifest.e76264a8f06bf333041d9ac96f5fb5a5.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "028a3e8c16567ff523ec4504258011ec",
+    "url": "./index.html"
+  },
+  {
+    "revision": "e359e47fdec76fc20f4b",
+    "url": "./static/css/main.28cc94ee.chunk.css"
+  },
+  {
+    "revision": "d9dcc89e47fea766eabb",
+    "url": "./static/js/2.03fd7a2f.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "./static/js/2.03fd7a2f.chunk.js.LICENSE"
+  },
+  {
+    "revision": "e359e47fdec76fc20f4b",
+    "url": "./static/js/main.0ad433c6.chunk.js"
+  },
+  {
+    "revision": "f8c0553bdb39d2c5ef40",
+    "url": "./static/js/runtime-main.ccd5c0ea.js"
+  },
+  {
+    "revision": "5c22fb1a69f532573aab485b91032747",
+    "url": "./static/media/codinglogo.5c22fb1a.svg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "./static/media/logo.5d5d9eef.svg"
+  }
+]);
