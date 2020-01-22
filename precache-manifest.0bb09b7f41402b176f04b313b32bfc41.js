2540ad29e330d57b2945af6a6c92e52db1fa785a
ethankeystone <ethankeystone@live.com>
2020-01-22 17:45:44 -0500

    fixing logo problem

---
+++ precache-manifest.0bb09b7f41402b176f04b313b32bfc41.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "347b7649d84eb3d84dac37f583143db3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a9b37102917a6c0ee33c",
+    "url": "/static/css/main.28cc94ee.chunk.css"
+  },
+  {
+    "revision": "3e981ca81ddda17698ba",
+    "url": "/static/js/2.ced29052.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/static/js/2.ced29052.chunk.js.LICENSE"
+  },
+  {
+    "revision": "a9b37102917a6c0ee33c",
+    "url": "/static/js/main.69890fad.chunk.js"
+  },
+  {
+    "revision": "27c01162644bf189bd60",
+    "url": "/static/js/runtime-main.e4876978.js"
+  },
+  {
+    "revision": "5c22fb1a69f532573aab485b91032747",
+    "url": "/static/media/codinglogo.5c22fb1a.svg"
+  }
+]);
